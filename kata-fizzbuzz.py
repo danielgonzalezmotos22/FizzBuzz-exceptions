3e7ddaea8c022dcffcb038318cf8bdd769f769c0
danielgonzalezmotos22 <danielgonzalezmotos22@gmail.com>
2026-01-12 12:57:56 +0100

    Segundo intento

--- kata-fizzbuzz.py
+++ kata-fizzbuzz.py
@@ -1,10 +1,22 @@
 def getnum():
-    print(input("diga un numero: "))
-
-def exceptions():
+    num = int(input("diga un numero: "))
+    return num
+def FizzBuzz():
+    num = getnum
+    if num %3 == 0:
+        print("Fizz")
+    elif num %5 == 0:
+        print("Buzz")
+    elif num %3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    else:
+        print(num)
+      
     # try:
-    #     getnum 
-    pass
+    #     num == int or float
+    # except:
+    #     print("Argumento no valido")
 
 if __name__ == "__main__":
     print(getnum())
+    print(FizzBuzz())
