3b1de97ecbed916067e9111883ea19ff5308b4bb
danielgonzalezmotos22 <danielgonzalezmotos22@gmail.com>
2026-01-13 11:04:27 +0100

    Tercer u¡intento

--- kata-fizzbuzz.py
+++ kata-fizzbuzz.py
@@ -1,22 +1,26 @@
 def getnum():
-    num = int(input("diga un numero: "))
-    return num
-def FizzBuzz():
-    num = getnum
-    if num %3 == 0:
-        print("Fizz")
+    # num = int(input("diga un numero: "))
+    
+    try:
+        num = int(input("diga un numero: "))
+    except Exception:
+        num = int(input("Argumento no valido, diga otro argumento: "))
+    # print (num)
+    if num %3 == 0 and num %5 == 0:
+        print("FizzBuzz")
     elif num %5 == 0:
         print("Buzz")
-    elif num %3 == 0 and num % 5 == 0:
-        print("FizzBuzz")
+    elif num %3 == 0:
+        print("Fizz")
     else:
         print(num)
+    return num
       
     # try:
-    #     num == int or float
-    # except:
+    #     num ==
+    # except ValueError:
     #     print("Argumento no valido")
 
 if __name__ == "__main__":
     print(getnum())
-    print(FizzBuzz())
+    # print(FizzBuzz())
