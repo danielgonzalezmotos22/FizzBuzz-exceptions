d3728cce28f6a9ae4bd2d766981190066cf2fd55
danielgonzalezmotos22 <danielgonzalezmotos22@gmail.com>
2026-01-19 11:59:06 +0100

    Casi hecho

--- kata_fizzbuzz.py
+++ kata_fizzbuzz.py
@@ -1,14 +1,5 @@
-def FizzBuzz():
-    i = 0
-    while i == 0:
-        try:
-            num = int(input("diga un numero entre 0 y 100: "))
-            if not num <=0 and num >= 101:
-                print(f"Numero invalido, diga otro numero: {num}")
-            else:
-                i += 1
-        except ValueError:
-            print("Diga otra cosa")
+def FizzBuzz(num):
+   
     if num %3 == 0 and num %5 == 0:
         return ("FizzBuzz")
     elif num %5 == 0:
@@ -21,5 +12,14 @@ def FizzBuzz():
       
 
 if __name__ == "__main__":
-
-    print(FizzBuzz())
+    i = 0
+    while i == 0:
+        try:
+            num = int(input("diga un numero entre 0 y 100: "))
+            if not num <=0 and num >= 101:
+                print(f"Numero invalido, diga otro numero: {num}")
+            else:
+                i += 1
+        except ValueError:
+            print("Diga otra cosa")
+    print(FizzBuzz(num))
--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,17 +1,37 @@
 from kata_fizzbuzz import FizzBuzz
 import pytest
-def num_div3():
+def test_num_div3():
     #Prueba si el numero es divisible entre 3 retorna "Fizz"
     assert FizzBuzz(3) == "Fizz"
 
-def num_div5():
+def test_num_div5():
     #Prueba si el numero es divisible entre 5 retorna "Buzz"
     assert FizzBuzz(5) == "Buzz"
 
-def num_div15():
+def test_num_div15():
     #Prueba si el numero es divisible entre 15 retorna "FizzBuzz"
     assert FizzBuzz(15) == "FizzBuzz"
 
-def si_numero_es_negativo():
+def test_si_numero_es_negativo():
     with pytest.raises(ValueError):
         FizzBuzz(-3)
+        
+def test_numero_mayor_a_cien():
+    with pytest.raises(ValueError):
+        FizzBuzz(101)
+
+def test_pasa_str():
+    with pytest.raises(TypeError):
+        FizzBuzz(str)
+
+def test_numeros_decimales():
+    with pytest.raises(TypeError):
+        FizzBuzz(float)
+
+def test_caracteres_especiales():
+    with pytest.raises(TypeError):
+        FizzBuzz("#")
+
+def test_pasar_mas_de_dos_numeros():
+    with pytest.raises(TypeError):
+        FizzBuzz(3, 5)
